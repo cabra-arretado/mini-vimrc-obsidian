28d624c231406f8160cb142ab98119ff3789c23e
Felipe <80273544+cabra-arretado@users.noreply.github.com>
2023-09-18 18:02:02 -0500

    more single responsability

--- main.ts
+++ main.ts
@@ -20,17 +20,20 @@ enum MapMode {
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 	private CodeMirrorVimObj: any = null;
-	vimrc_path: string = '.vimrc';
+	private vimrc_path: string = '.vimrc';
+	
 
 	//////////////////////////////////////////////////////////////
 	//////////////////////////////////////////////////////////////
 	//////////////////////////////////////////////////////////////
 	/* PLUGIN LOGIC HERE */
+
 	click_ribbon_icon() {
+		// Right now that is just the entrypoint for the plugin
 		if (this.CodeMirrorVimObj) {
 			this.process_vimrc();
 		}
-		new Notice('Ribbon icon clicked!');
+		new Notice('Loaded vimrc');
 	}
 
 	get_view(): MarkdownView | null {
@@ -38,27 +41,20 @@ export default class MyPlugin extends Plugin {
 	}
 
 	async process_vimrc(): Promise<void> {
+		/* Reads and executes one-by-one lines of the Vimrc file */
 		let file = await this.read_file(this.vimrc_path);
 		let lines = file.split('\n');
 		console.log("Processing vimrc file", lines.length, "lines");
 		for (let line of lines) {
 			this.process_line(line)
 		}
+		new Notice('vimrc loaded')
 	}
 
 	process_line(line: string): void {
-		if (line.length == 0) {
-			return
-		}
-		let line_arr = line.split(' ');
-		let mapMode = MapMode[line_arr[0] as keyof typeof MapMode].toString();
-		let lhs = line_arr[1];
-		let rhs = line_arr[2];
-		console.log("mapMode", mapMode, "lhs", lhs, "rhs", rhs);
-		this.set_vim_keybidding(lhs, rhs, mapMode);
-
+		/* Process a single line and runs the command there */
+		this.process_maps(line)
 	}
-
 
 	async read_file(path: string): Promise<string> {
 		try {
@@ -67,21 +63,44 @@ export default class MyPlugin extends Plugin {
 			return file;
 		}
 		catch (err) {
+			new Notice(`Could not find the file in ${path}`)
 			throw new Error(`Failed to read file ${path}`);
 		}
 	}
 
 	set_vim_keybidding(lhs: string, rhs: string, mode: string = 'normal') {
+		/* Set keybidings of imap, nmap, vmap */
 		(this.CodeMirrorVimObj as any).map(lhs, rhs, mode);
 		console.log(`set_vim_keybidding: ${lhs} -> ${rhs} in ${mode} mode`)
 	};
 
 	async initialize() {
+		/* Runs in the onload() */
 		this.CodeMirrorVimObj = (window as any).CodeMirrorAdapter?.Vim;
 		if (this.CodeMirrorVimObj) {
-			console.log("CodeMirrorVimObj exists");
+			// console.log('CMObj Present')
 		}
 	}
+
+	process_maps(line: string) {
+		let map_args = line.trim().split(' ');
+		//TODO: Check if it is on the enum
+		let mapMode = MapMode[map_args[0] as keyof typeof MapMode].toString();
+		if (!mapMode){
+			console.log('Could not map line.', line, '. There is no map command')
+			return
+		}
+		let lhs = map_args[1];
+		let rhs = map_args[2];
+		if (!lhs || !rhs){
+			console.log('Could not map line.', line, 'lhs or rhs not present')
+			return
+		}
+		console.log(`Successfully mapped! ${line}`)
+		this.set_vim_keybidding(lhs, rhs, mapMode);
+	}
+
+	
 	/* END PLUGIN LOGIC */
 	//////////////////////////////////////////////////////////////
 	//////////////////////////////////////////////////////////////
