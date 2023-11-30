68bc9d550b1914bc8a66e4ac82851332d33c616e
Felipe <80273544+cabra-arretado@users.noreply.github.com>
2023-11-29 21:45:30 -0600

    correct if statement + const variables

--- main.ts
+++ main.ts
@@ -22,17 +22,16 @@ export default class MiniVimrc extends Plugin {
 	private vimrcPath: string;
 
 	private async process_vimrc(): Promise<void> {
-		if (this.vimrcPath){
-
+		if (this.vimrcPath) {
+			/* Reads and executes one-by-one lines of the Vimrc file */
+			const file = await this.read_file(this.vimrcPath);
+			const lines = file.split('\n');
+			this.logger(`Processing vimrc file ${lines.length.toString()} lines`);
+			for (const line of lines) {
+				this.process_line(line);
+			}
+			new Notice('Vimrc loaded!')
 		}
-		/* Reads and executes one-by-one lines of the Vimrc file */
-		let file = await this.read_file(this.vimrcPath);
-		let lines = file.split('\n');
-		this.logger(`Processing vimrc file ${lines.length.toString()} lines`);
-		for (let line of lines) {
-			this.process_line(line);
-		}
-		new Notice('Vimrc loaded!')
 	}
 
 	private is_map(first_token: string): boolean {
@@ -42,12 +41,12 @@ export default class MiniVimrc extends Plugin {
 
 	private process_line(line: string): void {
 		/* Process a single line and runs the command there */
-		let trimmed_line = line.trim();
+		const trimmed_line = line.trim();
 		// Ignore comments and empty lines
 		if (trimmed_line.startsWith('"') || trimmed_line.length == 0) {
 			return
 		}
-		let line_tokens = trimmed_line.split(' ');
+		const line_tokens = trimmed_line.split(' ');
 		if (this.is_map(line_tokens[0])) {
 			this.process_maps(line_tokens);
 		}
@@ -60,7 +59,7 @@ export default class MiniVimrc extends Plugin {
 		/* The name says it all */
 		try {
 			this.logger(`Attempting to read vimrc file in path "${path}"`);
-			let file = await this.app.vault.adapter.read(path);
+			const file = await this.app.vault.adapter.read(path);
 			return file.trim();
 		}
 		catch (err) {
@@ -71,17 +70,17 @@ export default class MiniVimrc extends Plugin {
 
 	private set_vim_map(lhs: string, rhs: string, mode: string): void {
 		/* Set keybidings of map, imap, nmap, vmap */
-		let cmo = this.CodeMirrorVimObj as any;
+		const cmo = this.CodeMirrorVimObj as any;
 		this.logger(`set_vim_map: (${lhs} ${rhs} ${mode})`)
 		if (mode === MapMode['map']) {
 			cmo.map(lhs, rhs);
 			return
 		}
 		cmo.map(lhs, rhs, mode);
-	};
+	}
 
 	private set_vim_unmap(lhs: string): void {
-		let cmo = this.CodeMirrorVimObj as any;
+		const cmo = this.CodeMirrorVimObj as any;
 		this.logger(`set_vim_unmap: ${lhs}`)
 		cmo.unmap(lhs)
 	}
@@ -104,9 +103,9 @@ export default class MiniVimrc extends Plugin {
 
 	private process_maps(line_tokens: string[]) {
 		/* Process the map command */
-		let mapMode = MapMode[line_tokens[0] as keyof typeof MapMode].toString();
-		let lhs = line_tokens[1];
-		let rhs = line_tokens[2];
+		const mapMode = MapMode[line_tokens[0] as keyof typeof MapMode].toString();
+		const lhs = line_tokens[1];
+		const rhs = line_tokens[2];
 		if (mapMode === MapMode['unmap']) {
 			this.set_vim_unmap(lhs)
 			return
@@ -120,7 +119,7 @@ export default class MiniVimrc extends Plugin {
 
 	private logger(message: string): void {
 		/* To log messages to user */
-		let prefix = 'Mini Vimrc Plugin:';
+		const prefix = 'Mini Vimrc Plugin:';
 		console.log(prefix, message)
 	}
 
@@ -159,7 +158,7 @@ class SettingsTab extends PluginSettingTab {
 	}
 
 	display() {
-		let { containerEl } = this
+		const { containerEl } = this
 		containerEl.empty()
 
 		containerEl.createEl('h1', { text: 'Mini Vimrc Settings' })
